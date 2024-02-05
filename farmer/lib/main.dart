b3e972a34c84b9aa459fff8277d10eb71fe29018
Sharad Bisi <sharadbisi778@gmail.com>
2024-02-06 01:22:23 +0530

    new change

--- farmer/lib/main.dart
+++ farmer/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:farmer/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold()
+      home: const Homepage()
     );
   }
 }
